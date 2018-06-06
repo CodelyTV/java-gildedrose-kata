df1af4b5f0426e17e9e814d0a3ea097a220aeb07
Javier Ferrer <javier.mailserio@gmail.com>
2018-06-06 06:14:06 +0200

    Replace increments and decrements for expressions easier to read

--- src/main/java/tv/codely/kata/gildedrose/GildedRose.java
+++ src/main/java/tv/codely/kata/gildedrose/GildedRose.java
@@ -15,21 +15,21 @@ class GildedRose {
         for (Item item : items) {
             if (item.name.equals(AGED_BRIE)) {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                    item.quality += 1;
                 }
             } else if (item.name.equals(BACKSTAGE_PASSES)) {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                    item.quality += 1;
 
                     if (item.sellIn < 11) {
                         if (item.quality < 50) {
-                            item.quality = item.quality + 1;
+                            item.quality += 1;
                         }
                     }
 
                     if (item.sellIn < 6) {
                         if (item.quality < 50) {
-                            item.quality = item.quality + 1;
+                            item.quality += 1;
                         }
                     }
                 }
@@ -37,20 +37,20 @@ class GildedRose {
                 if (item.quality > 0) {
                     if (item.name.equals(SULFURAS)) {
                     } else {
-                        item.quality = item.quality - 1;
+                        item.quality -= 1;
                     }
                 }
             }
 
             if (item.name.equals(SULFURAS)) {
             } else {
-                item.sellIn = item.sellIn - 1;
+                item.sellIn -= 1;
             }
 
             if (item.sellIn < 0) {
                 if (item.name.equals(AGED_BRIE)) {
                     if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+                        item.quality += 1;
                     }
                 } else {
                     if (item.name.equals(BACKSTAGE_PASSES)) {
@@ -60,7 +60,7 @@ class GildedRose {
                             if (item.name.equals(SULFURAS)) {
                                 continue;
                             }
-                            item.quality = item.quality - 1;
+                            item.quality -= 1;
                         }
                     }
                 }
