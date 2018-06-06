3a926d4ab26d06d3ff649422a99fdcd18e5301aa
Javier Ferrer <javier.mailserio@gmail.com>
2018-06-06 06:02:19 +0200

    Invert if conditionals. Why: Easier to read

--- src/main/java/tv/codely/kata/gildedrose/GildedRose.java
+++ src/main/java/tv/codely/kata/gildedrose/GildedRose.java
@@ -13,14 +13,8 @@ class GildedRose {
 
     void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals(AGED_BRIE)
-                    && !item.name.equals(BACKSTAGE_PASSES)) {
-                if (item.quality > 0) {
-                    if (!item.name.equals(SULFURAS)) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
+            if (item.name.equals(AGED_BRIE)
+                    || item.name.equals(BACKSTAGE_PASSES)) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
@@ -38,26 +32,34 @@ class GildedRose {
                         }
                     }
                 }
+            } else {
+                if (item.quality > 0) {
+                    if (item.name.equals(SULFURAS)) {
+                    } else {
+                        item.quality = item.quality - 1;
+                    }
+                }
             }
 
-            if (!item.name.equals(SULFURAS)) {
+            if (item.name.equals(SULFURAS)) {
+            } else {
                 item.sellIn = item.sellIn - 1;
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE_PASSES)) {
+                if (item.name.equals(AGED_BRIE)) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                } else {
+                    if (item.name.equals(BACKSTAGE_PASSES)) {
+                        item.quality = 0;
+                    } else {
                         if (item.quality > 0) {
                             if (!item.name.equals(SULFURAS)) {
                                 item.quality = item.quality - 1;
                             }
                         }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
                     }
                 }
             }
