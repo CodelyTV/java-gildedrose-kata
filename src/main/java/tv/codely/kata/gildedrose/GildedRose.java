3a2e8b14a9ecfd0bca205413780ac90cecdd6ae7
Toni Peraira <toniperairam@gmail.com>
2022-10-29 12:51:49 +0200

    [my-solution-semantics] Replace if conditionals with switch

--- src/main/java/tv/codely/kata/gildedrose/GildedRose.java
+++ src/main/java/tv/codely/kata/gildedrose/GildedRose.java
@@ -2,61 +2,68 @@ package tv.codely.kata.gildedrose;
 
 class GildedRose {
 
-    private static final String ITEM_AGED_BRIE_NAME = "Aged Brie";
-    private static final String ITEM_BACKSTAGE_PASSES_NAME = "Backstage passes to a TAFKAL80ETC concert";
-    private static final String ITEM_SULFURAS_PASSES_NAME = "Sulfuras, Hand of Ragnaros";
-    Item[] items;
+  private static final String ITEM_AGED_BRIE_NAME = "Aged Brie";
+  private static final String ITEM_BACKSTAGE_PASSES_NAME = "Backstage passes to a TAFKAL80ETC concert";
+  private static final String ITEM_SULFURAS_PASSES_NAME = "Sulfuras, Hand of Ragnaros";
+  Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
-    }
+  public GildedRose(Item[] items) {
+    this.items = items;
+  }
 
-    public void updateQuality() {
-        for (Item item : items) {
-            if (item.name.equals(ITEM_AGED_BRIE_NAME)
-                    || item.name.equals(ITEM_BACKSTAGE_PASSES_NAME)) {
+  public void updateQuality() {
+    for (Item item : items) {
+      switch (item.name) {
+        case ITEM_AGED_BRIE_NAME:
+        case ITEM_BACKSTAGE_PASSES_NAME:
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+
+            if (item.name.equals(ITEM_BACKSTAGE_PASSES_NAME)) {
+              if (item.sellIn < 11) {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.name.equals(ITEM_BACKSTAGE_PASSES_NAME)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
+                  item.quality = item.quality + 1;
                 }
-            } else if (!item.name.equals(ITEM_SULFURAS_PASSES_NAME)) {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
+              }
+
+              if (item.sellIn < 6) {
+                if (item.quality < 50) {
+                  item.quality = item.quality + 1;
                 }
+              }
             }
+          }
+          break;
+        case ITEM_SULFURAS_PASSES_NAME:
+          break;
+        default:
+          if (item.quality > 0) {
+            item.quality = item.quality - 1;
+          }
+      }
 
-            if (!item.name.equals(ITEM_SULFURAS_PASSES_NAME)) {
-                item.sellIn = item.sellIn - 1;
+      if (!item.name.equals(ITEM_SULFURAS_PASSES_NAME)) {
+        item.sellIn = item.sellIn - 1;
+      }
+
+      if (item.sellIn < 0) {
+        switch (item.name) {
+          case ITEM_AGED_BRIE_NAME:
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
             }
-
-            if (item.sellIn < 0) {
-                if (item.name.equals(ITEM_AGED_BRIE_NAME)) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                } else if (item.name.equals(ITEM_BACKSTAGE_PASSES_NAME)) {
-                    item.quality = 0;
-                } else {
-                    if (item.quality > 0) {
-                        if (!item.name.equals(ITEM_SULFURAS_PASSES_NAME)) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                }
+            break;
+          case ITEM_BACKSTAGE_PASSES_NAME:
+            item.quality = 0;
+            break;
+          case ITEM_SULFURAS_PASSES_NAME:
+            break;
+          default:
+            if (item.quality > 0) {
+              item.quality = item.quality - 1;
             }
         }
+      }
     }
+  }
 }
