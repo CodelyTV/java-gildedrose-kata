842ec601731b0fb6d357e8ce01fb1d59a28771b7
Toni Peraira <toniperairam@gmail.com>
2022-10-29 13:14:55 +0200

    [my-solution-semantics] Add increase & decrease values functions

--- src/main/java/tv/codely/kata/gildedrose/GildedRose.java
+++ src/main/java/tv/codely/kata/gildedrose/GildedRose.java
@@ -67,4 +67,20 @@ class GildedRose {
       }
     }
   }
+
+  private void increaseQuality(Item item) {
+    if (item.quality < 50) {
+      item.quality += 1;
+    }
+  }
+
+  private void decreaseQuality(Item item) {
+    if (item.quality > 0) {
+      item.quality -= 1;
+    }
+  }
+
+  private void decreaseSellIn(Item item) {
+    item.sellIn -= 1;
+  }
 }
